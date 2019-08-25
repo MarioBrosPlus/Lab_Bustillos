a8f70e2064b01797d8b2012e724268e9742ccfbc
MarioBrosPlus <MarioBrosPlus@LAPTOP-TH1HA02O>
2019-08-24 19:00:26 -0500

    Se cambio Nuevamente

--- Lab_Bustillos/Lab_Bustillos/Acerca de.cs
+++ Lab_Bustillos/Lab_Bustillos/Acerca de.cs
@@ -21,7 +21,7 @@ namespace Lab_Bustillos
         {
             AyudaP.opcion = 3;
             new AyudaP().Show();
-            Console.WriteLine("Hola Mundo Mario");
+            Console.WriteLine("Hola Mundo Mario Etzael");
         }
     }
 }
